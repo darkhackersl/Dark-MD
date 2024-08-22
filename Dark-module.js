b8e71737680894e548818a1408c75f9b6b29c8e2
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-08-22 09:34:19 +0530

    Create Dark-module.js

---
+++ Dark-module.js
@@ -0,0 +1,8 @@
+//Shizo_modules importing system 
+import GIFBufferToVideoBuffer from './Dark/giftobuffer.js'
+import displayLoadingScreen from './Dark/loading.js'
+
+
+//Shizo_modules quick access system
+global.GIFBufferToVideoBuffer = GIFBufferToVideoBuffer
+global.displayLoadingScreen = displayLoadingScreen
